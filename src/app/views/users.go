b15caff0e8b5cf50364f4866bf3c5e7cb0974983
jeyem <me@e-mahmoudi.me>
2024-09-04 17:06:03 +0330

    get user

--- src/app/views/users.go
+++ src/app/views/users.go
@@ -14,7 +14,7 @@ func userGroup(router *gin.Engine) {
 	g := router.Group("users")
 	g.Use(auth.LoginRequired())
 
-	g.GET("/", func(c *gin.Context) {
+	g.GET("", func(c *gin.Context) {
 		u, _ := c.Get("user")
 		c.JSON(http.StatusOK, u)
 	})

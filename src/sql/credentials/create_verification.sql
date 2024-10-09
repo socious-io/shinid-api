6b76fd3f78f6be289d25c0eb4c8d1bf83fa4f3f9
jeyem <me@e-mahmoudi.me>
2024-10-09 11:52:46 +0330

    set default manually

--- src/sql/credentials/create_verification.sql
+++ src/sql/credentials/create_verification.sql
@@ -1,2 +1,2 @@
-INSERT INTO credential_verifications (name, description, user_id, schema_id) VALUES ($1, $2, $3, $4)
+INSERT INTO credential_verifications (name, description, user_id, schema_id, status) VALUES ($1, $2, $3, $4, 'CREATED')
 RETURNING *
--- src/sql/migrations/20241009110203_set_verification_default.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE credential_verifications
-ALTER COLUMN status SET DEFAULT 'CREATED';
